d1cd6cf168c66add900d2c3c2a9d952b8e92759b
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-06-08 00:37:07 +0800

    Move /usr/local/src back to ccgcommon.

--- manifests/rpmbuild.pp
+++ manifests/rpmbuild.pp
@@ -3,9 +3,4 @@ class profile::rpmbuild {
   include ccgcommon::source
   include repo::epel
   include repo::pgrpms
-
-  file { '/usr/local/src':
-    ensure => directory,
-    mode   => '0777',
-  }
 }
