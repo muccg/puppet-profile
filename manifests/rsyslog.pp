e5a88419b4d0b848b152f9370a373d006230ee5d
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-09-11 11:32:25 +0800

    Remove spurious vars from docs.

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -1,20 +1,16 @@
 #
 class profile::rsyslog {
-  class { 'rsyslog::client':
+  class { '::rsyslog::client':
     log_remote            => true,
     spool_size            => '1g',
     remote_type           => 'tcp',
-    remote_forward_format => 'RSYSLOG_ForwardFormat',
     log_local             => false,
     log_auth_local        => false,
     custom_config         => undef,
     custom_params         => undef,
     server                => 'log-syd.ec2.ccgapps.com.au',
     port                  => '514',
-    remote_servers        => false,
     ssl_ca                => undef,
-    log_templates         => false,
-    actionfiletemplate    => false
   }
 
   include monit
