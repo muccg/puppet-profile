9f36ca2fdf69f56ef6e4051bc53e1859ea08cbf6
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-11-23 15:03:14 +0800

    Allow puppet runmode to be set

--- manifests/sydney.pp
+++ manifests/sydney.pp
@@ -1,7 +1,11 @@
 #
-class profile::sydney {
+class profile::sydney (
+  $puppet_runmode = 'service',
+  ) {
   class { '::ccgcommon::environment::sydney': }
   class { '::ccgcommon::wgetrc::sydney': }
-  class { '::ccgcommon::puppet-client::sydney': }
   class { '::repo::sydney': }
+  class { '::ccgcommon::puppet-client::sydney': 
+    runmode => $puppet_runmode,
+  }
 }
